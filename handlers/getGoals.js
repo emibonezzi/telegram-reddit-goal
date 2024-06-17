9d872abc786b8855aa28572458ed7c4e40b2a0ef
emibonezzi <emibonezzi@gmail.com>
2024-06-17 19:46:28 -0400

    Add mongodb connection

---
+++ handlers/getGoals.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const goalSchema = new mongoose.Schema({
+  title: String,
+  url: String,
+});
+
+const Goal = mongoose.model("Goal", goalSchema);
+
+module.exports = async function () {
+  try {
+    // connect to database
+    await mongoose.connect(
+      `mongodb+srv://admin:${process.env.MONGO_DB_PASS}@goals.qejb4s7.mongodb.net/?retryWrites=true&w=majority&appName=Goals`
+    );
+    // retrieve all goals
+    const allGoals = await Goal.find();
+    return allGoals;
+  } catch (err) {
+    console.log("Error in dealing with db...");
+  }
+};
--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+const axios = require("axios");
+const getGoals = require("./handlers/getGoals");
 
-module.exports.handler = function (req, res) {
-  console.log(req);
+exports.handler = async (event, context) => {
+  try {
+    // Parse the event body
+    const body = JSON.parse(event.body);
+    console.log("Parsed body:", body);
+
+    // Retrieve from db list of goals
+    const goals = await getGoals();
+
+    console.log(goals);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "success",
+      }),
+    };
+  } catch (error) {
+    console.error("Error:", error.message);
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "Internal Server Error",
+        error: error.message,
+      }),
+    };
+  }
 };
